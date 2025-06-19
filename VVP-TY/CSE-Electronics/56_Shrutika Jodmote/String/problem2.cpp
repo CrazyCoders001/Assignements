0161e43591c19dc22af340d2233e3ebc28b732b2
Shrutikarj <163992772+Shrutikarj@users.noreply.github.com>
2025-06-19 13:48:06 +0530

    Create problem2.cpp

---
+++ VVP-TY/CSE-Electronics/56_Shrutika Jodmote/String/problem2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+// count word
+int main() {
+    string word;
+    int count = 0;
+    cout << "Enter sentence: ";
+    for(; cin >> word; count++) {
+       }
+    cout << "Word count:"<< count;
+}
