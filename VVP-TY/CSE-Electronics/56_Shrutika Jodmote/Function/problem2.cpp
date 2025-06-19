6cb4eeff2619a7f7923a5443956852c691c26bc8
Shrutikarj <163992772+Shrutikarj@users.noreply.github.com>
2025-06-19 14:22:30 +0530

    Create problem2.cpp

---
+++ VVP-TY/CSE-Electronics/56_Shrutika Jodmote/Function/problem2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+// multiply two num
+int multiply(int a, int b){
+    return a * b;
+}
+int main(){
+    int x, y;
+    cin>>x>>y;
+    cout<<multiply(x, y);
+}
