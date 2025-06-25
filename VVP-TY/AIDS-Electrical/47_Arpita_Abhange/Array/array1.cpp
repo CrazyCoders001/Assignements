c330e351a704db682673df1a5d6e61be003292fe
Arpita Abhange <102809687+Arpitaabhange01@users.noreply.github.com>
2025-06-25 10:12:23 +0530

    Update array1.cpp

--- VVP-TY/AIDS-Electrical/47_Arpita_Abhange/Array/array1.cpp
+++ VVP-TY/AIDS-Electrical/47_Arpita_Abhange/Array/array1.cpp
@@ -1,1 +1,36 @@
+//Count Positive, Negative, and Zero Elements
+//Problem:
+//Given an array nums, count how many elements are positive, negative, and zero.
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter elements in array: ";
+    cin>>n;
+
+    int nums[n];
+    cout<<"Enter elements:\n";
+
+    for (int i = 0; i < n; i++) {
+        cin>>nums[i];
+    }
+
+    int positive = 0, negative = 0, zero = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] > 0) {
+            positive++;
+        } else if (nums[i] < 0) {
+            negative++;
+        } else {
+            zero++;
+        }
+    }
+
+    cout<<"Positive elements: "<<positive<<endl;
+    cout<<"Negative elements: "<<negative<<endl;
+    cout<<"Zero elements: "<<zero<<endl;
+
+}
