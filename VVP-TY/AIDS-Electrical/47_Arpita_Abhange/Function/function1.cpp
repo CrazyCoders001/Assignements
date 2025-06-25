cb378e7dc3acc54d77ef09ecbd73e9823cbf7ad2
Arpita Abhange <102809687+Arpitaabhange01@users.noreply.github.com>
2025-06-25 10:30:06 +0530

    Update function1.cpp

--- VVP-TY/AIDS-Electrical/47_Arpita_Abhange/Function/function1.cpp
+++ VVP-TY/AIDS-Electrical/47_Arpita_Abhange/Function/function1.cpp
@@ -1,1 +1,22 @@
+//Problem 1: Check Even or Odd
+//Problem Statement:
+//Write a function that takes an integer as input and returns whether it is even or odd.
 
+#include<bits/stdc++.h>
+using namespace std;
+
+void checkEvenOdd(int number) {
+    if (number % 2 == 0) {
+        cout<<number<<" is Even"<<endl;
+    } else {
+        cout<<number<<" is Odd"<<endl;
+    }
+}
+
+int main() {
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+
+    checkEvenOdd(num);
+}
