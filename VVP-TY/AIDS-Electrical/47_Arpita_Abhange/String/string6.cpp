1428500834fe6a88046b27b9ab9e009725f25dcb
Arpita Abhange <102809687+Arpitaabhange01@users.noreply.github.com>
2025-06-25 10:45:35 +0530

    Create string6.cpp

---
+++ VVP-TY/AIDS-Electrical/47_Arpita_Abhange/String/string6.cpp
@@ -0,0 +1,15 @@
+//Capitalize Each Word
+//Problem:
+//Given a sentence s, capitalize the first letter of each word and return the modified string
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+ string s ="Arpita";
+ cout<<s<<;
+
+ cout<<(string)toupper(s);
+ return 0;
+}
