991807a526488afff12ff8a68447919df80eebc5
Arpita Abhange <102809687+Arpitaabhange01@users.noreply.github.com>
2025-06-25 10:43:10 +0530

    Update string2.cpp

--- VVP-TY/AIDS-Electrical/47_Arpita_Abhange/String/string2.cpp
+++ VVP-TY/AIDS-Electrical/47_Arpita_Abhange/String/string2.cpp
@@ -1,1 +1,26 @@
+//Count Words in a Sentence
+//Problem:
+//Given a sentence string s, count and return the number of words. Words are separated by spaces.
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string inpString;
+    cout<<"Enter Input string :";
+    cin>>inpString;
+
+    getline(cin, inpString);
+
+    int spaceCount = 0;
+    for(int i=0; i<inpString.length(); i++){
+        char ch = inpString[i];
+        if(ch == ' '){
+            spaceCount++;
+        }
+    }
+
+    int wordCount = spaceCount + 1;
+    cout<<"Word Count = "<<wordCount;
+}
