2cbd133e46dc6a93bf352ffc6ecf8bc4107ec7a9
Arpita Abhange <102809687+Arpitaabhange01@users.noreply.github.com>
2025-06-25 10:15:30 +0530

    Update array2.cpp

--- VVP-TY/AIDS-Electrical/47_Arpita_Abhange/Array/array2.cpp
+++ VVP-TY/AIDS-Electrical/47_Arpita_Abhange/Array/array2.cpp
@@ -1,1 +1,34 @@
+//Find Index of Target Element
+//Problem:
+//Given an array nums and a target value x, return the index of the first occurrence of x. If not found, return -1.
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cout<<"Enter of elements in array: ";
+    cin>>n;
+
+    int nums[n];
+    cout<<"Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin>>nums[i];
+    }
+
+    int x;
+    cout<<"Enter target value : ";
+    cin >> x;
+
+    int index = -1; 
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == x) {
+            index = i;
+            break; 
+        }
+    }
+
+    cout<<"Index of first occurrence of " <<x<< ": " <<index<<endl;
+
+    return 0;
+}
