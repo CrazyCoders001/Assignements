6d4f27a7c018573591b321f3bd222e3ea3f41119
Itz-vaishu <148346870+Itz-vaishu@users.noreply.github.com>
2025-06-26 13:42:11 +0530

    Update stack1.cpp

--- VVP-TY/AIDS-Electrical/31_Vaishnavi Charate/Stack/stack1.cpp
+++ VVP-TY/AIDS-Electrical/31_Vaishnavi Charate/Stack/stack1.cpp
@@ -1,1 +1,33 @@
+//Write a program that uses a stack to reverse a given string.
+//💡 Sample Input:"hello"
+//📤 Output:"olleh"
+#include <iostream>
+#include <stack>
+using namespace std;
+// Function to reverse a string using a stack
+string reverseString(string input) 
+{
+    stack<char> s;
+    // Push each character into the stack
+    for (char ch : input) 
+    {
+        s.push(ch);
+    }
+    // Pop characters from the stack to reverse the string
+    string reversed = "";
+    while (!s.empty()) 
+    {
+        reversed += s.top();
+        s.pop();
+    }
+
+    return reversed;
+}
+int main() 
+{
+    string str = "hello";  // input string
+    cout << "Input: " << str << endl;
+    cout << "Reversed: " << reverseString(str) << endl; // function call
+    return 0;
+}
 
