564aabd5977135933f09c1c7c453709708976caf
Itz-vaishu <148346870+Itz-vaishu@users.noreply.github.com>
2025-06-26 12:40:20 +0530

    Update function1.cpp

--- VVP-TY/AIDS-Electrical/31_Vaishnavi Charate/Function/function1.cpp
+++ VVP-TY/AIDS-Electrical/31_Vaishnavi Charate/Function/function1.cpp
@@ -1,1 +1,16 @@
-
+//Write a function that takes an integer as input and returns whether it is even or odd.
+#include <iostream>
+using namespace std;
+string checkEvenOdd(int n) // User-defined function to check even or odd
+{
+    if (n % 2 == 0)
+        return "Even";
+    else
+        return "Odd";
+}
+int main() // Main function to test the logic
+{
+    int number = 7;  //  value
+    cout << "Input: " << number<< " ,Output: " << checkEvenOdd(number) << endl; //function call
+    return 0;
+}
