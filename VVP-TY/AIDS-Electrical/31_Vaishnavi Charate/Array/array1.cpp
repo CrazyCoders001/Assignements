68272d0e8bce3b4d2927992b243b25d604de4a3c
Itz-vaishu <148346870+Itz-vaishu@users.noreply.github.com>
2025-06-26 13:50:39 +0530

    Update array1.cpp

--- VVP-TY/AIDS-Electrical/31_Vaishnavi Charate/Array/array1.cpp
+++ VVP-TY/AIDS-Electrical/31_Vaishnavi Charate/Array/array1.cpp
@@ -1,1 +1,5 @@
+//1. Count Positive, Negative, and Zero Elements
+//Problem:Given an array nums, count how many elements are positive, negative, and zero.
+//Input: nums = [1, -2, 0, 5, -1, 0] , Output: Positive: 2, Negative: 2, Zero: 2
+//Input: nums = [-5, -3, 2] , Output: Positive: 1, Negative: 2, Zero: 0
 
